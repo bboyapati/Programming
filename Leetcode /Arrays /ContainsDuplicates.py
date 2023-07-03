8a7baaedfe4d9ba611e45a5d3fc67e3ffec3a9c2
Bharath C Boyapati <62618863+bboyapati@users.noreply.github.com>
2023-07-03 21:32:40 +0100

    Update ContainsDuplicates.py
    Updated problem statement

--- Leetcode /Arrays /ContainsDuplicates.py
+++ Leetcode /Arrays /ContainsDuplicates.py
@@ -1,1 +1,19 @@
-Goteem
+"""
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct
+
+Input: nums = [1,2,3,1]
+Output: true
+
+Input: nums = [1,2,3,4]
+Output: false
+"""
+
+    
+
+
+
+
+
+
+
+
