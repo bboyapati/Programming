ed991d7fd4650f20347494011038c4461f681b22
Bharath C Boyapati <62618863+bboyapati@users.noreply.github.com>
2023-07-04 09:11:23 +0100

    Update ContainsDuplicates.py
    Solution added

--- Leetcode /Arrays /ContainsDuplicates.py
+++ Leetcode /Arrays /ContainsDuplicates.py
@@ -7,6 +7,22 @@ Output: true
 Input: nums = [1,2,3,4]
 Output: false
 """
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            else:
+                hashset.add(n)
+        return False
+"""
+Create a set as it can't have duplicate items
+Iterate through through array
+if integer in array is also in set, duplicate found and return TRUE
+Otherwise, add the element to the list
+"""
 
     
 
